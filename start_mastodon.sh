75664edcc5c60e5e06db9de6d1795199cfee796f
Mariano Dominguez <mariano.dm@gmail.com>
2023-02-05 16:05:38 -0800

    document startup

--- start_mastodon.sh
+++ start_mastodon.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-#uncomment for testing, this shouls be already running
+#uncomment for testing or in docker, this should be already existing in bm
 
-echo "109814700946549490" > sinceFile.txt
-nohup Xvfb :99 > /dev/null 2>&1 &
+#echo "109808029753084484" > sinceFile.txt
+#nohup Xvfb :99 > /dev/null 2>&1 &
+
+#Add credentials here
+
+#export MASTODON_SERVER="https://oldbytes.space/"
+#export CONSUMER_KEY=""
+#export CONSUMER_SECRET=""
+#export ACCESS_TOKEN=""
 
 python3 AtariBotMastodon.py
