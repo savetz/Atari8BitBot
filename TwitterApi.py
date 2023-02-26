416423531d89eec6c651aa5a0ff344f3cfed2d31
Mariano Dominguez <mariano.dm@gmail.com>
2023-02-25 21:38:29 -0800

    fix tw api null

--- TwitterApi.py
+++ TwitterApi.py
@@ -14,15 +14,15 @@ class TwitterApi:
     def get_api(Self, consumer_key, consumer_secret, access_token, access_token_secret):
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth, wait_on_rate_limit=True,
+        Self.api = tweepy.API(auth, wait_on_rate_limit=True,
             wait_on_rate_limit_notify=True)
         try:
-            api.verify_credentials()
+            Self.api.verify_credentials()
         except Exception as e:
             Self.logger.error("Error creating API", exc_info=True)
             raise e
         Self.logger.info("API created")
-        return api
+        return Self.api
 
     def media_upload(Self,filename):
         media = Self.api.media_upload(filename)
