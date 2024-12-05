1cc9efb72522b8674ee5c693df35d2480c69b14f
Papá Robot <mariano.dm@gmail.com>
2024-12-04 16:18:24 -0800

    Update TestBluesky.py

--- TestBluesky.py
+++ TestBluesky.py
@@ -1,6 +1,20 @@
-from blueskysocial import Client, Post, Video
-c=Client()
-c.authenticate("papa-robot.bsky.social", "4jN8t4ZZfKZUmR7f9fpE")
-video = Video('path/to/video.mov')
-post = Post('Video Post', with_attachments=video)
-c.post(post)
+
+from atproto import Client
+from atproto_client import models
+
+def search_mentions(handle):
+    results = []
+    response = client.app.bsky.feed.search_posts(
+            params = models.AppBskyFeedSearchPosts.Params(
+                q=f"{handle}"
+            )
+    )
+    results.extend(response.posts)
+    return results
+
+
+client = Client()
+client.login('papa-robot.bsky.social', 'no tokens here')
+mentions = search_mentions("#atari8bitbot")
+for post in mentions:
+    print(post.record.text)
